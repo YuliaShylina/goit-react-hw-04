ca8e53e108490fffb1adca1a510e40918a00498a
YuliaShylina <yuliyashylina@gmail.com>
2024-07-12 09:24:38 +0300

    second

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,28 +1,7 @@
-// import css from "./ImageGallery.module.css";
-// import ImageCard from "../ImageCard/ImageCard";
-
-// export default function ImageGallery({ images, onImageClick }) {
-//   return (
-//     <ul className={css.list}>
-//       {images.map((data, index) => (
-//         <li className={css.item} key={`${data.id}-${index}`}>
-//           <ImageCard
-//             src={data.urls.small}
-//             alt={data.alt_description}
-//             onClick={() => onImageClick(data.id)} // Добавляем обработчик клика здесь
-//           />
-//         </li>
-//       ))}
-//     </ul>
-//   );
-// }
-
 import ImageCard from "../ImageCard/ImageCard";
 import css from "./ImageGallery.module.css";
 
 export default function ImageGallery({ images, onImgClick }) {
-  console.log("onImgClick:", onImgClick);
-
   return (
     <ul className={css.list}>
       {images.map((data, index) => (
--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,54 +1,11 @@
-// import css from "./ImageModal.module.css";
-// import { IoIosCloseCircleOutline } from "react-icons/io";
-// import { BiLike } from "react-icons/bi";
-// import { format } from "date-fns";
-// import { useEffect } from "react";
-
-// export default function ImageModal({
-//   isOpen,
-//   closeModal,
-//   imageUrl,
-//   altDescription,
-//   likes,
-//   createdAt,
-//   description,
-// }) {
-//   useEffect(() => {
-//     if (isOpen) {
-//       document.body.style.overflow = "hidden";
-//     } else {
-//       document.body.style.overflow = "auto";
-//     }
-//   }, [isOpen]);
-
-//   if (!isOpen) return null;
-
-//   return (
-//     <div className={css.overlay} onClick={closeModal}>
-//       <div className={css.content} onClick={(e) => e.stopPropagation()}>
-//         <img src={imageUrl} alt={altDescription} className={css.image} />
-//         <div className={css.info}>
-//           <p>
-//             <BiLike size={24} />
-//             Likes: {likes}
-//           </p>
-//           <p>Created at: {format(new Date(createdAt), "Pp")}</p>
-//           {description && <p>Description: {description}</p>}
-//         </div>
-//         <button onClick={closeModal} className={css.closeBtn}>
-//           <IoIosCloseCircleOutline size={40} />
-//         </button>
-//       </div>
-//     </div>
-//   );
-// }
-
 import React from "react";
 import ReactModal from "react-modal";
 import { IoIosCloseCircleOutline } from "react-icons/io";
 import { BiLike } from "react-icons/bi";
 import { format } from "date-fns";
 import css from "./ImageModal.module.css";
+
+ReactModal.setAppElement("#root");
 
 export default function ImageModal({ isOpen, onRequestClose, img }) {
   return (
